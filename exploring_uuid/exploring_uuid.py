93f238430dca3c22d3ddf8e8a19565925e458173
Simon Gsponer <simon.gsponer@gmail.com>
2020-03-01 21:35:43 +0100

    added further test code to repo

--- exploring_uuid/exploring_uuid.py
+++ exploring_uuid/exploring_uuid.py
@@ -1,7 +1,26 @@
+import logging
+from pathlib import Path
 
+def initialize_logger():
+    logger_fp = Path("logs/logging_info.json")
+
+    logger = logging.getLogger("uuid-4-experiment-logger")
+    logger.setLevel(logging.INFO)
+
+    fh = logging.FileHandler(logger_fp)
+
+    formatter = logging.Formatter('{"timestamp": "%(asctime)s", "name": "%(name)s", "levelname": "%(levelname)s", "message": "%(message)s"}')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    return logger
 
 def main():
-    print("hello world")
+
+    logger = initialize_logger()
+
+    for i in range(5):
+        logger.info('creating an instance of auxiliary_module.Auxiliary')
 
 if __name__ == '__main__':
     main()
